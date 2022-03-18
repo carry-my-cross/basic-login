45499e37651abf21daebd6867091d9ed868860ef
carry-my-cross <dlthreet@mail.fhsu.edu>
2022-03-17 21:33:00 -0500

    add ability to take user input

--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 
-class Test {
+class UserLogin {
 public:
-    void test () {
+    std::string userName;
+    std::string userPass;
+
+    void test() {
         std::cout << "I am in the test function" << std::endl;
+    }
+
+    void set_userName (UserLogin& user) {
+        std::cout << "enter username: " ;
+        std::cin >> user.userName;
+        std:: cout << user.userName << std::endl;
+    }
+
+    void set_userPass (UserLogin& user) {
+        std::cout << "enter password: " ;
+        std::cin >> user.userPass;
+        std:: cout << user.userPass << std::endl;
     }
 };
 
 int main() {
-    Test myTest;
-    myTest.test();
+    UserLogin user;
+    user.set_userName(user);
+    user.set_userPass(user);
+
     return 0;
 }
